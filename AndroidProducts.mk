f3d85dba871d3db8c6b138340500c0f32471f728
TheHitMan7 <krtik.vrma@gmail.com>
2021-03-24 22:31:45 +0530

    ginkgo: Initial NitrogenOS bringup
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nitrogen_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    nitrogen_ginkgo-user \
+    nitrogen_ginkgo-userdebug \
+    nitrogen_ginkgo-eng
